7ed2977c33707f7393beb5868711e6f1d406e7d6
Jeff Haynie <jhaynie@appcelerator.com>
2014-05-23 14:01:00 -0700

    more work

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+var hl = require('./lib/hyperloop'),
+	_ = require('underscore');
 
-module.exports = {
+module.exports = _.extend(hl,{
 	compiler: require('./lib/compiler'),
 	log: require('./lib/log'),
 	config: require('./lib/config'),
 	util: require('./lib/util'),
 	hook: require('./lib/hook'),
-	unit: require('./lib/unit')
-};
+	unit: require('./lib/unit'),
+	spinner: require('./lib/spinner'),
+	Command: require('./lib/command')
+});
+
